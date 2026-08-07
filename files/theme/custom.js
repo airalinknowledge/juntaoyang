e43d42a529c262d3ac97936af71ed6cf349d878c
airalinknowledge <154879942+airalinknowledge@users.noreply.github.com>
2026-08-07 17:02:59 +0800

    Update contact email sitewide

--- files/theme/custom.js
+++ files/theme/custom.js
@@ -322,3 +322,62 @@
     initializeSiteAdditions();
   }
 })();
+
+(function () {
+  "use strict";
+
+  var oldEmail = "juntao.yang@columbia.edu";
+  var newEmail = "info@juntaoyang.site";
+
+  function replaceContactEmail(root) {
+    root = root || document.body;
+    if (!root) return;
+
+    var walker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT, null, false);
+    var nodes = [];
+    while (walker.nextNode()) {
+      if (walker.currentNode.nodeValue && walker.currentNode.nodeValue.indexOf(oldEmail) !== -1) {
+        nodes.push(walker.currentNode);
+      }
+    }
+
+    for (var i = 0; i < nodes.length; i++) {
+      nodes[i].nodeValue = nodes[i].nodeValue.split(oldEmail).join(newEmail);
+    }
+
+    var links = root.querySelectorAll ? root.querySelectorAll('a[href^="mailto:"]') : [];
+    for (var j = 0; j < links.length; j++) {
+      var href = links[j].getAttribute("href") || "";
+      if (href.indexOf(oldEmail) !== -1) {
+        links[j].setAttribute("href", href.split(oldEmail).join(newEmail));
+      }
+    }
+  }
+
+  function initializeEmailReplacement() {
+    replaceContactEmail(document.body);
+
+    if (!document.body || typeof MutationObserver === "undefined") return;
+    var observer = new MutationObserver(function (mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        for (var j = 0; j < mutations[i].addedNodes.length; j++) {
+          var node = mutations[i].addedNodes[j];
+          if (node.nodeType === 3) {
+            if (node.nodeValue && node.nodeValue.indexOf(oldEmail) !== -1) {
+              node.nodeValue = node.nodeValue.split(oldEmail).join(newEmail);
+            }
+          } else if (node.nodeType === 1) {
+            replaceContactEmail(node);
+          }
+        }
+      }
+    });
+    observer.observe(document.body, { childList: true, subtree: true });
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", initializeEmailReplacement);
+  } else {
+    initializeEmailReplacement();
+  }
+})();
